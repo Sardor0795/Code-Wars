12a3d8f66c98dcfdd23ecf939540744e2664b113
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2022-11-21 17:48:22 +0500

    Find Maximum and Minimum 8kyu

--- ex_21-11.js
+++ ex_21-11.js
@@ -3,3 +3,25 @@
 // function isLeapYear(year) {
 //   return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
 // }
+
+// Find Maximum and Minimum Values of a List -- 8kyu
+
+// const min = (list) => {
+//   let min = list[0];
+//   for (let i = 0; i < list.length; i++) {
+//     if (list[i] < min) {
+//       min = list[i];
+//     }
+//   }
+//   return min;
+// };
+
+// const max = (list) => {
+//   let max = list[0];
+//   for (let i = 0; i < list.length; i++) {
+//     if (list[i] > max) {
+//       max = list[i];
+//     }
+//   }
+//   return max;
+// };
