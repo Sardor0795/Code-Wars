9e70c012df7e7d5df9baad8ea92e11c1160a7b7f
Sardor0795 <english.programmer.9595@gmail.com>
2022-11-28 19:20:52 +0500

    Square Every Digit  7 kyu

---
+++ ex_28-11.js
@@ -0,0 +1,14 @@
+// function squareDigits(num) {
+//     num = String(num);
+
+//     let arrNum = num.split("");
+//     let sqArr = [];
+
+//     for (let i = 0; i < arrNum.length; i++) {
+//       sqArr.push(arrNum[i] ** 2);
+//     }
+
+//     let sqNum = sqArr.join("");
+
+//     return +sqNum;
+//   }
