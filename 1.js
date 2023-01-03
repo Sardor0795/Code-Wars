f4a9e9766dc06e999570317e2fc4f64f8065a885
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-01-03 11:09:23 +0500

    8 kyu - finalGrade

---
+++ 1.js
@@ -0,0 +1,15 @@
+// Student's Final Grade --- 8 kyu
+
+// function finalGrade(exam, projects) {
+//   if (exam > 90 || projects > 10) return 100;
+
+//   if (exam > 75 && projects >= 5) return 90;
+
+//   if (exam > 50 && projects >= 2) return 75;
+
+//   return 0;
+// }
+
+// const finalGrade = (exam, projects) => exam > 90 || projects > 10 ? 100 : exam > 75 && projects >= 5 ? 90 : exam > 50 && projects >= 2 ? 75 : 0;
+
+// console.log(finalGrade(70, 5));
