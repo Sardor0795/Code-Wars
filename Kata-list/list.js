94b3b586a29a6fafbc46dc964bc48788535aed42
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-01-03 17:04:25 +0500

    Kata list

--- Kata-list/list.js
+++ Kata-list/list.js
@@ -2,3 +2,4 @@
 // 2. Century From Year - 8kyu - https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097/train/javascript
 // 3. Mumbling - 7kyu - https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/train/javascript
 // 4. Decode the Morse code - 6 kyu - https://www.codewars.com/kata/54b724efac3d5402db00065e/train/javascript
+// 5. Binary Addition - 7 kyu - https://www.codewars.com/kata/551f37452ff852b7bd000139/train/javascript
