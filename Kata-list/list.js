c9d1b42e82e5c9502cd264fd5b59a2c192a8778b
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-01-03 13:27:03 +0500

    Add links

--- Kata-list/list.js
+++ Kata-list/list.js
@@ -1,2 +1,2 @@
-// 1. Student's Final Grade - 8 kyu
-// 2. Century From Year - 8kyu
+// 1. Student's Final Grade - 8 kyu - https://www.codewars.com/kata/5ad0d8356165e63c140014d4/train/javascript
+// 2. Century From Year - 8kyu - https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097/train/javascript
