a03d7f979ed79d87038f00b3364caa5aac635625
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-01-03 16:40:25 +0500

    Morse Code

---
+++ 1.js
@@ -0,0 +1,48 @@
+// |--1-- Student's Final Grade - 8 kyu
+
+// function finalGrade(exam, projects) {
+//   if (exam > 90 || projects > 10) return 100;
+
+//   if (exam > 75 && projects >= 5) return 90;
+
+//   if (exam > 50 && projects >= 2) return 75;
+
+//   return 0;
+// }
+
+// const finalGrade = (exam, projects) => exam > 90 || projects > 10 ? 100 : exam > 75 && projects >= 5 ? 90 : exam > 50 && projects >= 2 ? 75 : 0;
+
+// console.log(finalGrade(70, 5));
+
+// |--2-- Century From Year - 8kyu (decade)
+
+// const century = (year) => year % 100 !== 0 ? Math.floor(year / 100) + 1 : year / 100; // 1st way
+
+// const century = (year) => Math.ceil(year / 100); // 2nd way
+
+// console.log(century(2023));
+
+// |--3-- Mumbling - 7 kyu (built in)
+
+// const accum = (s) => {
+//   let res = "";
+//   for (let i = 0; i < s.length; i++) {
+//     for (let j = 0; j < i + 1; j++) {
+//       if (j == 0) {
+//         res += s[i].toUpperCase();
+//       } else {
+//         res += s[i].toLowerCase();
+//       }
+//     }
+//     if (i != s.length - 1) {
+//       res += "-";
+//     }
+//   }
+//   return res;
+// };
+
+// console.log(accum("absd"));
+
+// |--4-- Decode the Morse code - 6 kyu
+
+// const decodeMorse = (morseCode) => morseCode.split('   ').map(word => word.split(' ').map(code => MORSE_CODE[code]).join('')).join(' ').trim()
--- Kata-list/list.js
+++ Kata-list/list.js
@@ -1,2 +1,4 @@
 // 1. Student's Final Grade - 8 kyu - https://www.codewars.com/kata/5ad0d8356165e63c140014d4/train/javascript
 // 2. Century From Year - 8kyu - https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097/train/javascript
+// 3. Mumbling - 7kyu - https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/train/javascript
+// 4. Decode the Morse code - 6 kyu - https://www.codewars.com/kata/54b724efac3d5402db00065e/train/javascript
